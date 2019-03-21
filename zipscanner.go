5c75151267da1e43476a0c9e92a476d6f3912dc8
Joe Mann <contact@joemann.co.uk>
2019-03-21 20:52:00 +0100

    Initial zip scanner.

---
+++ zipscanner.go
@@ -0,0 +1,139 @@
+package shapefile
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"strings"
+	"sync"
+
+	"github.com/mercatormaps/go-shapefile/dbf"
+	"github.com/pkg/errors"
+)
+
+type ZipScanner struct {
+	in   *zip.Reader
+	name string
+
+	initOnce sync.Once
+	scanner  *Scanner
+}
+
+func NewZipScanner(r io.ReaderAt, size int64, filename string) (*ZipScanner, error) {
+	in, err := zip.NewReader(r, size)
+	if err != nil {
+		return nil, err
+	}
+
+	if !strings.HasSuffix(filename, ".zip") {
+		return nil, fmt.Errorf("expecting name to be *.zip")
+	}
+
+	return &ZipScanner{
+		in:   in,
+		name: strings.TrimSuffix(filename, ".zip"),
+	}, nil
+}
+
+func (s *ZipScanner) Info() (*Info, error) {
+	if err := s.init(); err != nil {
+		return nil, err
+	}
+	return s.scanner.Info()
+}
+
+func (s *ZipScanner) Scan(opts ...dbf.Option) error {
+	if err := s.init(); err != nil {
+		return err
+	}
+	return s.scanner.Scan(opts...)
+}
+
+func (s *ZipScanner) Record() *Record {
+	if s.scanner == nil {
+		return nil
+	}
+	return s.scanner.Record()
+}
+
+func (s *ZipScanner) Err() error {
+	if s.scanner == nil {
+		return nil
+	}
+	return s.scanner.Err()
+}
+
+func (s *ZipScanner) init() error {
+	var err error
+
+	s.initOnce.Do(func() {
+		var shpFile, dbfFile, cfgFile *zip.File
+		shpFile, dbfFile, cfgFile, err = s.files()
+		if err != nil {
+			return
+		}
+		_ = cfgFile
+
+		var shpR, dbfR io.ReadCloser
+		shpR, err = shpFile.Open()
+		if err != nil {
+			err = errors.Wrapf(err, "failed to open %s", shpFile.Name)
+			return
+		}
+
+		dbfR, err = dbfFile.Open()
+		if err != nil {
+			err = errors.Wrapf(err, "failed to open %s", dbfFile.Name)
+			return
+		}
+
+		s.scanner = NewScanner(shpR, dbfR)
+	})
+
+	return err
+}
+
+func (s *ZipScanner) files() (shpFile, dbfFile, cpgFile *zip.File, err error) {
+	if s.name != "" {
+		for _, f := range s.in.File {
+			switch f.Name {
+			case s.name + ".shp":
+				shpFile = f
+			case s.name + ".dbf":
+				dbfFile = f
+			case s.name + ".cpg":
+				cpgFile = f
+			}
+		}
+	} else {
+		for _, f := range s.in.File {
+			switch {
+			case strings.HasSuffix(f.Name, ".shp"):
+				if shpFile != nil {
+					err = fmt.Errorf("found multiple .shp files")
+					return
+				}
+				shpFile = f
+			case strings.HasSuffix(f.Name, ".dbf"):
+				if dbfFile != nil {
+					err = fmt.Errorf("found multiple .dbf files")
+					return
+				}
+				dbfFile = f
+			case strings.HasSuffix(f.Name, "cpg"):
+				if cpgFile != nil {
+					err = fmt.Errorf("found multiple .cpg files")
+					return
+				}
+				cpgFile = f
+			}
+		}
+	}
+
+	if shpFile == nil {
+		err = fmt.Errorf("missing .shp file")
+	} else if dbfFile == nil {
+		err = fmt.Errorf("missing .dbf file")
+	}
+	return
+}
